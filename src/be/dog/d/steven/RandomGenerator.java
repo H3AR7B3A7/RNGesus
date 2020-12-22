18a7d9c7f7e3a0dd6eaf7b9699fdbb451fb511dd
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2020-12-22 12:36:32 +0100

    :children_crossing: Automatic reset when out of numbers

--- src/be/dog/d/steven/RandomGenerator.java
+++ src/be/dog/d/steven/RandomGenerator.java
@@ -6,27 +6,33 @@ import java.util.Random;
 
 public class RandomGenerator {
 
-    private final ArrayList<Integer> randomList;
+    private ArrayList<Integer> randomList;
     private final int size;
     private final Random r = new Random();
     private int last = 0;
 
     public RandomGenerator(int size) {
-
-        randomList = new ArrayList<>();
+        randomList = getRandomList();
         this.size = size;
+    }
+
+    public ArrayList<Integer> getRandomList() {
+        randomList = new ArrayList<>();
 
         for (int i = 1; i <= size; i++) {
             randomList.add(i);
         }
 
         Collections.shuffle(randomList);
+
+        return randomList;
     }
 
     public String readAndRemoveNextRandom() {
-        if (randomList.size() > 0) {
-            return randomList.remove(0).toString();
-        } else return null;
+        if (randomList.size() <= 0) {
+            randomList = getRandomList();
+        }
+        return randomList.remove(0).toString();
     }
 
     public String readNextRandomAndShuffle() {
